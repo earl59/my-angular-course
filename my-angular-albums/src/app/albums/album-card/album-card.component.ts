8284696a6ab48aebdc6bbea8d8e3bb1ba6e88d0b
Earl Spangenberg <espange@netscape.net>
2019-07-31 11:07:02 -0400

    new code

---
+++ my-angular-albums/src/app/albums/album-card/album-card.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { getLocaleNumberSymbol } from '@angular/common';
+
+import { Album } from '../album.model';
+
+@Component({
+  selector: 'app-album-card',
+  templateUrl: './album-card.component.html',
+  styleUrls: ['./album-card.component.css']
+})
+export class AlbumCardComponent implements OnInit {
+  @Input()
+  album: Album;
+ 
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
